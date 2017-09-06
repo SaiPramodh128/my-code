1270bc116c500a02023ca33440d86de1b8f6bf6d
Sai Pramodh <31702408+SaiPramodh128@users.noreply.github.com>
2017-09-06 23:07:06 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+public static void main(String srgs[]){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=Sc.nextInt();
+int c=a+b;
+System.out.println(c);
+}
+}
