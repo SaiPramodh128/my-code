c0aeb22da16fccbcb2890fea606a7cbe05e22649
Sai Pramodh <31702408+SaiPramodh128@users.noreply.github.com>
2017-09-06 23:14:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test{
 public static void main(String srgs[]){
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
-int b=Sc.nextInt();
+int b=sc.nextInt();
 int c=a+b;
 System.out.println(c);
 }
